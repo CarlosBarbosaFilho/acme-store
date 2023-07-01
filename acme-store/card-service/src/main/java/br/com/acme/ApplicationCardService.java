33c846edc63045bfaf282f53c43c2dba6db3fc44
Professor <professor@debian-BULLSEYE-live-builder-AMD64>
2023-07-01 14:01:31 -0300

    create docker compose: card service

--- acme-store/card-service/src/main/java/br/com/acme/ApplicationCardService.java
+++ acme-store/card-service/src/main/java/br/com/acme/ApplicationCardService.java
@@ -3,6 +3,7 @@ package br.com.acme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApplicationCardService {
     public static void main(String[] args) {
--- acme-store/card-service/src/main/java/br/com/acme/adapters/output/database/h2/repository/CardRepository.java
+++ acme-store/card-service/src/main/java/br/com/acme/adapters/output/database/h2/repository/CardRepository.java
@@ -2,6 +2,8 @@ package br.com.acme.adapters.output.database.h2.repository;
 
 import br.com.acme.adapters.output.database.h2.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -11,4 +13,7 @@ import java.util.List;
 public interface CardRepository extends JpaRepository <Card, Long> {
 
     List<Card> findByIncomeLessThanEqual(BigDecimal income);
+
+    @Query(value = "select * from cards where income <= :income", nativeQuery = true)
+    List<Card> returnListCardsByIncome(@Param("income") String income);
 }
